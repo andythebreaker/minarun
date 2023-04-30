940a1ec86684521acd14ca6babd9b97ec57ff8bb
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-04-30 12:24:48 +0800

    ?

--- src/App.js
+++ src/App.js
@@ -11,10 +11,12 @@ import { Fab, Action } from 'react-tiny-fab';
 import 'react-tiny-fab/dist/styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MySwal = withReactContent(Swal);
 
-var wtf=MySwal.fire({
+var wtf = MySwal.fire({
   title: <p>此應用程式為預覽版，開發者不對使用者所產生的任何後果負責。使用者應該自行審慎評估應用程式的功能和風險，並自負所有風險及責任。我們強烈建議使用者小心使用此應用程式，並盡可能避免在生產環境中使用。使用此應用程式的風險完全由使用者自行承擔。</p>,
   didOpen: () => {
     // `MySwal` is a subclass of `Swal` with all the same instance & static methods
@@ -25,33 +27,37 @@ var wtf=MySwal.fire({
 });
 
 function App() {
+
+  const notify = () => { toast("Wow so easy!"); console.log("???"); };
+
   return (
     <div className="App">
       <Fab
-  //mainButtonStyles={mainButtonStyles}
-  //actionButtonStyles={actionButtonStyles}
-  //style={style}
-  icon={<FontAwesomeIcon icon={icon({name: 'bars'})} />}
-  //event={event}
-  alwaysShowTitle={true}
-  onClick={wtf}
->
-  <Action
-    text="Email"
-    onClick={wtf}
-  ><FontAwesomeIcon icon={icon({name: 'key'})} /> </Action>
-  <Action
-      text="Help"
-      onClick={wtf}
-    >
-<FontAwesomeIcon icon={icon({name: 'user-secret'})} />  </Action>
-</Fab>
-        <div className="net-work-ind">
-           <div>不管有沒有網路都會顯示的內容</div>
-           <Online>有網路時會顯示的內容</Online>
-           <Offline>離線時會顯示的內容</Offline>
-       </div>
-<Demo></Demo>
+        //mainButtonStyles={mainButtonStyles}
+        //actionButtonStyles={actionButtonStyles}
+        //style={style}
+        icon={<FontAwesomeIcon icon={icon({ name: 'bars' })} />}
+        //event={event}
+        alwaysShowTitle={true}
+        onClick={notify}
+      >
+        <Action
+          text="Email"
+          onClick={wtf}
+        ><FontAwesomeIcon icon={icon({ name: 'key' })} /> </Action>
+        <Action
+          text="Help"
+          onClick={wtf}
+        >
+          <FontAwesomeIcon icon={icon({ name: 'user-secret' })} />  </Action>
+      </Fab>
+      <div className="network-indicator">
+        <div>不管有沒有網路都會顯示的內容</div>
+        <Online>有網路時會顯示的內容</Online>
+        <Offline>離線時會顯示的內容</Offline>
+      </div>
+      <Demo></Demo>
+      <ToastContainer />
     </div>
   );
 }
--- src/Demo.js
+++ src/Demo.js
@@ -1,56 +1,70 @@
-import React ,{useState,useMemo}from "react";
+import React, { useState, useMemo } from "react";
 import { useGeolocated } from "react-geolocated";
-import { MapContainer, TileLayer,Marker,Popup, useMap,Rectangle} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap, Rectangle } from 'react-leaflet';
+import L from "leaflet";
 import 'leaflet/dist/leaflet.css';
+import './css/map_gps.css'
+
+const customIcon = L.icon({
+    iconUrl: "./logo.svg",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowUrl: "./logo.svg",
+    shadowSize: [41, 41],
+    shadowAnchor: [12, 41],
+  });
+
 const innerBounds = [
     [49.505, -2.09],
     [53.505, 2.09],
-  ]
-  const outerBounds = [
+]
+const outerBounds = [
     [50.505, -29.09],
     [52.505, 29.09],
-  ]
-  
-  const redColor = { color: 'red' }
-  const whiteColor = { color: 'white' }
-  function SetBoundsRectangles() {
+]
+
+const redColor = { color: 'red' }
+const whiteColor = { color: 'white' }
+function SetBoundsRectangles() {
     const [bounds, setBounds] = useState(outerBounds)
     const map = useMap()
-  
+
     const innerHandlers = useMemo(
-      () => ({
-        click() {
-          setBounds(innerBounds)
-          map.fitBounds(innerBounds)
-        },
-      }),
-      [map],
+        () => ({
+            click() {
+                setBounds(innerBounds)
+                map.fitBounds(innerBounds)
+            },
+        }),
+        [map],
     )
     const outerHandlers = useMemo(
-      () => ({
-        click() {
-          setBounds(outerBounds)
-          map.fitBounds(outerBounds)
-        },
-      }),
-      [map],
+        () => ({
+            click() {
+                setBounds(outerBounds)
+                map.fitBounds(outerBounds)
+            },
+        }),
+        [map],
     )
-  
+
     return (
-      <>
-        <Rectangle
-          bounds={outerBounds}
-          eventHandlers={outerHandlers}
-          pathOptions={bounds === outerBounds ? redColor : whiteColor}
-        />
-        <Rectangle
-          bounds={innerBounds}
-          eventHandlers={innerHandlers}
-          pathOptions={bounds === innerBounds ? redColor : whiteColor}
-        />
-      </>
+        <>
+            <Rectangle
+                bounds={outerBounds}
+                eventHandlers={outerHandlers}
+                pathOptions={bounds === outerBounds ? redColor : whiteColor}
+            />
+            <Rectangle
+                bounds={innerBounds}
+                eventHandlers={innerHandlers}
+                pathOptions={bounds === innerBounds ? redColor : whiteColor}
+            />
+        </>
     )
-  }
+}
 const Demo = () => {
 
     const { coords, isGeolocationAvailable, isGeolocationEnabled } =
@@ -61,55 +75,55 @@ const Demo = () => {
             userDecisionTimeout: 5000,
         });
 
-        if (!isGeolocationAvailable) {
-            return <div>Your browser does not support Geolocation</div>;
-        } else if (!isGeolocationEnabled) {
-            return <div>Geolocation is not enabled</div>;
-        } else if (coords) {
-            return (
-                <div>
-                    <MapContainer style={{ width: "100vw", height: "100vh" }} bounds={outerBounds} zoom={13} scrollWheelZoom={false}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={[51.505, -0.09]}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                        </Marker>
-                        <SetBoundsRectangles />
-                    </MapContainer>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>latitude</td>
-                                <td>{coords.latitude}</td>
-                            </tr>
-                            <tr>
-                                <td>longitude</td>
-                                <td>{coords.longitude}</td>
-                            </tr>
-                            <tr>
-                                <td>altitude</td>
-                                <td>{coords.altitude}</td>
-                            </tr>
-                            <tr>
-                                <td>heading</td>
-                                <td>{coords.heading}</td>
-                            </tr>
-                            <tr>
-                                <td>speed</td>
-                                <td>{coords.speed}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            );
-        } else {
-            return <div>Getting the location data&hellip; </div>;
-        }
-        
+    if (!isGeolocationAvailable) {
+        return <div>Your browser does not support Geolocation</div>;
+    } else if (!isGeolocationEnabled) {
+        return <div>Geolocation is not enabled</div>;
+    } else if (coords) {
+        return (
+            <div>
+                <MapContainer style={{ width: "100vw", height: "100vh" }} center={[coords.latitude, coords.longitude]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[coords.latitude, coords.longitude]} icon={customIcon}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                    <SetBoundsRectangles />
+                </MapContainer>
+                <table className="ld-float">
+                    <tbody>
+                        <tr>
+                            <td>latitude</td>
+                            <td>{coords.latitude}</td>
+                        </tr>
+                        <tr>
+                            <td>longitude</td>
+                            <td>{coords.longitude}</td>
+                        </tr>
+                        <tr>
+                            <td>altitude</td>
+                            <td>{coords.altitude}</td>
+                        </tr>
+                        <tr>
+                            <td>heading</td>
+                            <td>{coords.heading}</td>
+                        </tr>
+                        <tr>
+                            <td>speed</td>
+                            <td>{coords.speed}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    } else {
+        return <div>Getting the location data&hellip; </div>;
+    }
+
 };
 
 export default Demo;
