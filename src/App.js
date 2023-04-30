aeae27d62f33e0699fe49e4c8ee6d00632107a4b
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-04-30 09:49:35 +0800

    okuifloat

--- src/App.js
+++ src/App.js
@@ -2,18 +2,56 @@
 import './App.css';
 import { Online, Offline } from 'react-detect-offline';
 import Demo from './Demo.js';
+//import { Float } from '@headlessui-float/react'
+import 'semantic-ui-css/semantic.min.css';
+//import { Card } from 'semantic-ui-react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { Fab, Action } from 'react-tiny-fab';
+import 'react-tiny-fab/dist/styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+const MySwal = withReactContent(Swal);
+
+var wtf=MySwal.fire({
+  title: <p>Hello World</p>,
+  didOpen: () => {
+    // `MySwal` is a subclass of `Swal` with all the same instance & static methods
+    MySwal.showLoading()
+  },
+}).then(() => {
+  return MySwal.fire(<p>Shorthand works too</p>)
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<Demo></Demo>
-        <div>
+      <Fab
+  //mainButtonStyles={mainButtonStyles}
+  //actionButtonStyles={actionButtonStyles}
+  //style={style}
+  //icon={<MdAdd />}
+  //event={event}
+  alwaysShowTitle={true}
+  onClick={wtf}
+>
+  <Action
+    text="Email"
+    onClick={wtf}
+  ><FontAwesomeIcon icon={icon({name: 'key'})} /> </Action>
+  <Action
+      text="Help"
+      onClick={wtf}
+    >
+<FontAwesomeIcon icon={icon({name: 'user-secret'})} />  </Action>
+</Fab>
+        <div className="net-work-ind">
            <div>不管有沒有網路都會顯示的內容</div>
            <Online>有網路時會顯示的內容</Online>
            <Offline>離線時會顯示的內容</Offline>
        </div>
-      </header>
+<Demo></Demo>
     </div>
   );
 }
--- src/Demo.js
+++ src/Demo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useGeolocated } from "react-geolocated";
+import { MapContainer, TileLayer,Marker,Popup, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const Demo = () => {
     const { coords, isGeolocationAvailable, isGeolocationEnabled } =
@@ -10,38 +12,54 @@ const Demo = () => {
             userDecisionTimeout: 5000,
         });
 
-    return !isGeolocationAvailable ? (
-        <div>Your browser does not support Geolocation</div>
-    ) : !isGeolocationEnabled ? (
-        <div>Geolocation is not enabled</div>
-    ) : coords ? (
-        <table>
-            <tbody>
-                <tr>
-                    <td>latitude</td>
-                    <td>{coords.latitude}</td>
-                </tr>
-                <tr>
-                    <td>longitude</td>
-                    <td>{coords.longitude}</td>
-                </tr>
-                <tr>
-                    <td>altitude</td>
-                    <td>{coords.altitude}</td>
-                </tr>
-                <tr>
-                    <td>heading</td>
-                    <td>{coords.heading}</td>
-                </tr>
-                <tr>
-                    <td>speed</td>
-                    <td>{coords.speed}</td>
-                </tr>
-            </tbody>
-        </table>
-    ) : (
-        <div>Getting the location data&hellip; </div>
-    );
+        if (!isGeolocationAvailable) {
+            return <div>Your browser does not support Geolocation</div>;
+        } else if (!isGeolocationEnabled) {
+            return <div>Geolocation is not enabled</div>;
+        } else if (coords) {
+            return (
+                <div>
+                    <MapContainer style={{ width: "100vw", height: "100vh" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>latitude</td>
+                                <td>{coords.latitude}</td>
+                            </tr>
+                            <tr>
+                                <td>longitude</td>
+                                <td>{coords.longitude}</td>
+                            </tr>
+                            <tr>
+                                <td>altitude</td>
+                                <td>{coords.altitude}</td>
+                            </tr>
+                            <tr>
+                                <td>heading</td>
+                                <td>{coords.heading}</td>
+                            </tr>
+                            <tr>
+                                <td>speed</td>
+                                <td>{coords.speed}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        } else {
+            return <div>Getting the location data&hellip; </div>;
+        }
+        
 };
 
 export default Demo;
