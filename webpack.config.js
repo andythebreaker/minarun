3e8789b314a85f0abf064605aff90eacf1076206
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-04-30 13:38:07 +0800

    ?

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+const Dotenv = require( 'dotenv-webpack' );
+const { InjectManifest } = require( 'workbox-webpack-plugin' );
+const CopyPlugin = require( 'copy-webpack-plugin' );
+
+const webpackPlugins = [
+  new HtmlWebpackPlugin( {
+    template: path.resolve( __dirname, 'public/index.html' ),
+    filename: 'index.html',
+  } ),
+  new Dotenv( {
+    path: './.env', // Path to .env file (this is the default)
+    systemvars: true,
+  } ),
+  new CopyPlugin( {
+    patterns: [
+      { from: './src/favicon.ico', to: '' },
+      { from: './src/manifest.json', to: '' },
+      { from: './src/logo192.png', to: '' },
+      { from: './src/logo512.png', to: '' },
+    ],
+  } ),
+];
+
+if ( 'production' === process.env.NODE_ENV ) {
+  webpackPlugins.push( new InjectManifest( {
+    swSrc: './src/src-sw.js',
+    swDest: 'sw.js',
+  } ) );
+}
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve( __dirname, 'dist' ),
+    filename: 'main.js',
+    publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css?$/,
+        use: [ 'style-loader', 'css-loader' ],
+      },
+      /*{
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader?name=./images/[name].[ext]',
+      },{
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+      },*/
+    ],
+  },
+  plugins: webpackPlugins,
+};
+
