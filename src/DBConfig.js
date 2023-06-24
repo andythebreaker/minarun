ddd90a66fa8309c722c283ab438422ee3eba6df9
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-06-24 22:43:18 +0800

    * package

--- src/DBConfig.js
+++ src/DBConfig.js
@@ -3,11 +3,12 @@ export const DBConfig = {
     version: 1,
     objectStoresMeta: [
       {
-        store: 'people',
+        store: 'gps',
         storeConfig: { keyPath: 'id', autoIncrement: true },
         storeSchema: [
-          { name: 'name', keypath: 'name', options: { unique: false } },
-          { name: 'email', keypath: 'email', options: { unique: false } }
+          { name: 'time', keypath: 'time', options: { unique: false } },
+          { name: 'latitude', keypath: 'latitude', options: { unique: false } },
+          { name: 'longitude', keypath: 'longitude', options: { unique: false } }
         ]
       }
     ]
