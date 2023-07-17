db2d9c575148e211f5e3710bfcc883cefec93b69
andythebreaker <e24074724@mail.ncku.edu.tw>
2023-07-17 23:53:28 +0800

    clean code

--- scripts/test.js
+++ scripts/test.js
@@ -1,64 +1,11 @@
-/*'use strict';
-
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'test';
-process.env.NODE_ENV = 'test';
-process.env.PUBLIC_URL = '';
-
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-  throw err;
-});
-
-// Ensure environment variables are read.
-require('../config/env');
-
-const jest = require('jest');
-const execSync = require('child_process').execSync;
-let argv = process.argv.slice(2);
-
-function isInGitRepository() {
-  try {
-    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function isInMercurialRepository() {
-  try {
-    execSync('hg --cwd . root', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-// Watch unless on CI or explicitly running all tests
-if (
-  !process.env.CI &&
-  argv.indexOf('--watchAll') === -1 &&
-  argv.indexOf('--watchAll=false') === -1
-) {
-  // https://github.com/facebook/create-react-app/issues/5210
-  const hasSourceControl = isInGitRepository() || isInMercurialRepository();
-  argv.push(hasSourceControl ? '--watch' : '--watchAll');
-}
-
-
-jest.run(argv);
-*/
 const puppeteer = require('puppeteer');
 
 async function runTest() {
-  // Launch a headless Chrome browser
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
   try {
+    // Launch a headless Chrome browser
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
     // Enable the 'geolocation' permission
     const context = browser.defaultBrowserContext();
     await context.overridePermissions('https://andythebreaker.github.io/minarun', ['geolocation']);
@@ -74,7 +21,6 @@ async function runTest() {
 
     // Navigate to the PWA
     await page.goto('https://andythebreaker.github.io/minarun');
-    //await page.waitForNavigation();
 
     // Enable geolocation and set a random location
     await page.evaluate(() => {
@@ -127,11 +73,11 @@ async function runTest() {
     // Wait for a few seconds before closing the browser
     await page.waitForTimeout(2000);
 
-  } catch (error) {
-    console.error('Test failed:', error);
-  } finally {
     // Close the browser
     await browser.close();
+
+  } catch (error) {
+    console.error('Test failed:', error);
   }
 }
 
