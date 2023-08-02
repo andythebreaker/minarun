1f1e724050bae88dc7adee3a351774e2222a91a4
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2023-08-02 19:13:35 +0800

    Update Demo.js

--- src/Demo.js
+++ src/Demo.js
@@ -111,7 +111,7 @@ const Demo = (props) => {
         storeLocationData(coords);
         add({ latitude: coords.latitude, longitude: coords.longitude }).then(
             event0 => {
-              console.log('ID Generated: ', event0.target.result);
+              console.log('ID Generated[???]: ', event0);
               if (event0.target.result - 2 >= 0) {
                 deleteRecord(event0.target.result - 2).then(event1 => {
                   console.log('Deleted!');
